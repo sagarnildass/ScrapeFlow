4bb264e6b62b0ae93142e33efb5c33b5cf224cf4
Sagarnil Das <sagarnildass@gmail.com>
2025-04-17 16:32:02 +0530

    implemented openai

--- lib/workflow/executor/ExtractDataWithAiExecutor.ts
+++ lib/workflow/executor/ExtractDataWithAiExecutor.ts
@@ -2,6 +2,7 @@ import { ExecutionEnvironment } from "@/types/executor";
 import { ExtractDataWithAITask } from "../task/ExtractDataWithAI";
 import { prisma } from "@/lib/prisma";
 import { symmetricDecrypt } from "@/lib/encryption";
+import { OpenAI } from "openai";
 
 export async function ExtractDataWithAiExecutor(
   environment: ExecutionEnvironment<typeof ExtractDataWithAITask>
@@ -47,14 +48,44 @@ export async function ExtractDataWithAiExecutor(
       return false;
     }
     
-    const mockExtractedData = {
-        usernameSelector: "#username",
-        passwordSelector: "#password",
-        loginSelector: "body > div > form > input.btn.btn-primary",
+    const openai = new OpenAI({
+      apiKey: plainCredentialValue,
+    })
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        {
+          role: "system",
+          content: "You are a webscraper helper that extracts data from HTML or text. You will be given a piece of text or HTML content \
+          as input and also the prompt with the data you have to extract. The response should always be only the extracted data as a JSON array \
+          or object, without any additional words or explanations. Analyze the input carefully and extract data precisely based on the prompt. \
+          If no data is found, return an empty JSON array. Work only with the provided content and ensure the output is always a valid \
+          JSON array without any surrounding text.",
+        },
+        {
+          role: "user",
+          content: content,
+        },
+        {
+          role: "user",
+          content: prompt,
+        },
+      ],
+      temperature: 1
+    });
+    
+    environment.log.info(`Prompt Tokens, ${response.usage?.prompt_tokens}`);
+    environment.log.info(`Completion Tokens, ${response.usage?.completion_tokens}`);
+
+    const result = response.choices[0].message?.content;
+
+    if (!result) {
+      environment.log.error("Empty response from AI");
+      return false;
     }
 
-    environment.setOutput("Extracted data", JSON.stringify(mockExtractedData));
-    console.log("@@PLAIN CREDENTIAL VALUE", plainCredentialValue);
+    environment.setOutput("Extracted data", result);
 
     return true;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
